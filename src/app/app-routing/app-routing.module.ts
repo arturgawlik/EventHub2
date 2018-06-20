f04bf426936d3f3a859c8a8e5c1e660f604fdc5c
arturgawlik <artur.gawlik@icloud.com>
2018-06-21 00:06:48 +0200

    fix route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeDashboardComponent } from '../home-dashboard/home-dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: HomeDashboardComponent },
+  { path: 'home', component: HomeDashboardComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
