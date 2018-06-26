735c3b7dbd01bb29c22cdf4f574969629f34e855
arturgawlik <artur.gawlik@icloud.com>
2018-06-26 22:24:55 +0200

    auth fix v2

--- src/app/Routing/app-routing/app-routing.module.ts
+++ src/app/Routing/app-routing/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { HomeDashboardComponent } from '../../Components/home-dashboard/home-dashboard.component';
 import { AddEventComponent } from '../../Components/add-event/add-event.component';
+import { AuthService } from 'src/app/Services/auth/auth.service';
 
 const routes: Routes = [
   { path: '', component: HomeDashboardComponent },
@@ -13,4 +14,28 @@ const routes: Routes = [
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  private isLoggedIn: boolean;
+  private user_displayName: string;
+  private user_email: string;
+
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.af.authState.subscribe((auth) => {
+      if (auth == null) {
+        // not logged in
+        this.isLoggedIn = false;
+        this.user_displayName = '';
+        this.user_email = '';
+        this.router.navigate(['Login']);
+      } else {
+        // logged in
+        this.isLoggedIn = true;
+        this.user_displayName = auth.displayName;
+        this.user_email = auth.email;
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+}
