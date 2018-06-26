d026bdf96bdae4b50104a0036a609da81035a2aa
arturgawlik <artur.gawlik@icloud.com>
2018-06-26 19:47:54 +0200

    auth fix bug

--- src/app/Services/auth/auth.service.ts
+++ src/app/Services/auth/auth.service.ts
@@ -1,73 +1,74 @@
-// import { Injectable } from '@angular/core';
-// import {AngularFireAuth} from 'angularfire2/auth';
-// import { firebase } from '@firebase/app';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-
-//   constructor(public af: AngularFireAuth) { }
-
-//   doFacebookLogin() {
-//     return new Promise<any>((resolve, reject) => {
-//       // tslint:disable-next-line:prefer-const
-//       let provider = new firebase.auth.FacebookAuthProvider();
-//       this.af.auth
-//         .signInWithPopup(provider)
-//         .then(res => {
-//           resolve(res);
-//         }, err => {
-//           console.log(err);
-//           reject(err);
-//         });
-//     });
-//   }
+import { Injectable } from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import firebase from '@firebase/app';
 
 
-//   doGoogleLogin() {
-//     return new Promise<any>((resolve, reject) => {
-//       // tslint:disable-next-line:prefer-const
-//       let provider = new firebase.auth.GoogleAuthProvider();
-//       provider.addScope('profile');
-//       provider.addScope('email');
-//       this.af.auth
-//         .signInWithPopup(provider)
-//         .then(res => {
-//           resolve(res);
-//         }, err => {
-//           console.log(err);
-//           reject(err);
-//         });
-//     });
-//   }
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
 
-//   doRegister(value) {
-//     return new Promise<any>((resolve, reject) => {
-//       firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
-//         .then(res => {
-//           resolve(res);
-//         }, err => reject(err));
-//     });
-//   }
+  constructor(public af: AngularFireAuth) { }
 
-//   doLogin(value) {
-//     return new Promise<any>((resolve, reject) => {
-//       firebase.auth().signInWithEmailAndPassword(value.email, value.password)
-//         .then(res => {
-//           resolve(res);
-//         }, err => reject(err));
-//     });
-//   }
+  doFacebookLogin() {
+    return new Promise<any>((resolve, reject) => {
+      // tslint:disable-next-line:prefer-const
+      let provider = new firebase.auth.FacebookAuthProvider();
+      this.af.auth
+        .signInWithPopup(provider)
+        .then(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
 
-//   doLogout() {
-//     return new Promise((resolve, reject) => {
-//       if (firebase.auth().currentUser) {
-//         this.af.auth.signOut();
-//         resolve();
-//       } else {
-//         reject();
-//       }
-//     });
-//   }
-// }
+
+  doGoogleLogin() {
+    return new Promise<any>((resolve, reject) => {
+      // tslint:disable-next-line:prefer-const
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+      provider.addScope('email');
+      this.af.auth
+        .signInWithPopup(provider)
+        .then(res => {
+          resolve(res);
+        }, err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
+
+  doRegister(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(value.email, value.password)
+        .then(res => {
+          resolve(res);
+        }, err => reject(err));
+    });
+  }
+
+  doLogin(value) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(value.email, value.password)
+        .then(res => {
+          resolve(res);
+        }, err => reject(err));
+    });
+  }
+
+  doLogout() {
+    return new Promise((resolve, reject) => {
+      if (firebase.auth().currentUser) {
+        this.af.auth.signOut();
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
+}
